b520596ae5b74e3918fa6326c7115c3a74a2feaf
rsharma2024 <159127988+rsharma2024@users.noreply.github.com>
2024-02-20 09:52:39 +0100

    fix

--- ascii-script.sh
+++ ascii-script.sh
@@ -1,5 +1,4 @@
 #/bin/sh
 sudo apt-get install cowsay -y
-cowsay -f  dragon "Run for cover, I am dragon....RAWR" >> dragon.txt
-grep -i "dragon" dragon.txt
-cat dragon.txt
+cowsay -f  tux "I Love Linux" >> Hello.txt
+cat Hello.txt
